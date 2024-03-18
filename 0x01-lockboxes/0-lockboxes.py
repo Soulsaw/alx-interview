dd8c9e0e94e45ab19f5405a6f404ed5137c2628d
Soulsaw <ssawadogo292@gmail.com>
2024-03-18 01:32:40 +0000

    delete

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,2 +1,23 @@
-#!/usr/bin/env python3
-"""Doc module"""
+#!/usr/bin/python3
+"""Doc of the module"""
+
+
+def canUnlockAll(boxes):
+    """This function if all the box can be open"""
+    if not boxes or len(boxes) == 0:
+        return False
+
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+
+    queue = [0]
+
+    while queue:
+        current_box = queue.pop(0)
+        for key in boxes[current_box]:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                queue.append(key)
+
+    return all(visited)
