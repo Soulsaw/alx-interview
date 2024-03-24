2b5a1f28a0a634c43d3931f7d8df67521f1b170d
Soulsaw <ssawadogo292@gmail.com>
2024-03-24 22:11:09 +0000

    fix the challenge

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Doc module"""
+
+
+def minOperations(n: int) -> int:
+    """This function compute the minimum operation"""
+    if n == 1:
+        return 0
+    dp = [0] * (n + 1)
+    dp[1] = 1  # The default H character
+    for i in range(2, n + 1):
+        dp[i] = i
+        for j in range(2, i):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + (i // j))
+    return dp[n]
