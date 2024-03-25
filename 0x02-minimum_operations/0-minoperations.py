4813448d9666681b485424adca5de5b066080f42
Soulsaw <ssawadogo292@gmail.com>
2024-03-25 08:02:55 +0000

    fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,11 +7,9 @@ def minOperations(n):
     if n <= 0:
         return 0
 
-    dp = [float('inf')] * (n + 1)
-
-    dp[1] = 0  # The default H character
-
+    dp = {1: 0}
     for i in range(2, n + 1):
+        dp[i] = float('inf')
         for j in range(1, i):
             if i % j == 0:
                 dp[i] = min(dp[i], dp[j] + (i // j))
