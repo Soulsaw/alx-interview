b2fd1630fbb4f07389866999fcaf8e40040cf25b
Soulsaw <ssawadogo292@gmail.com>
2024-08-12 00:21:32 +0000

    fix all

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
 
-const getCharactersFromMovies = (movieId) => {
+const getCharactersFromMovies = async (movieId) => {
   const baseUrl = 'https://swapi-api.alx-tools.com/api/';
-  request(`${baseUrl}films`, function (error, reponse, body) {
+  await request(`${baseUrl}films`, function (error, reponse, body) {
     if (error) return;
     const films = JSON.parse(body).results;
     if (movieId < 1 || movieId > films.lenght) return;
     const film = films[movieId - 1];
     const characters = film.characters;
 
-    characters.map(url => request(url, function (err, resp, body) {
+    characters.map(async url => await request(url, function (err, resp, body) {
       if (err) return;
       const character = JSON.parse(body);
       console.log(character.name);
