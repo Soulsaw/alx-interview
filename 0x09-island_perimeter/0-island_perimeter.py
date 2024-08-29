884594a45051700ef156d96d025494067301c469
Soulsaw <ssawadogo292@gmail.com>
2024-08-29 22:18:33 +0000

    fix the island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,18 +6,14 @@ def island_perimeter(grid) -> int:
     """This function is aim to determinate the island perimeter"""
     perimeter = 0
     for i in range(len(grid)):
-        if i == 0 or i == len(grid) - 1:
-            continue
         for j in range(len(grid[i])):
-            if j == 0 or j == len(grid[i]) - 1:
-                continue
             if (grid[i][j] == 1):
-                if grid[i - 1][j] == 0:
+                if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-                if grid[i + 1][j] == 0:
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
-                if grid[i][j - 1] == 0:
+                if j == 0 or grid[i][j - 1] == 0:
                     perimeter += 1
-                if grid[i][j + 1] == 0:
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
     return perimeter
