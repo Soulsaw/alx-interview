62d538348c5b725bfa92d492c2d39d49c1e65b6f
Soulsaw <ssawadogo292@gmail.com>
2024-12-02 06:05:27 +0000

    fix the island perimeter challange project

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""Doc of the island perimeter module"""
+"""Doc to handle is land perimeter"""
 
 
 def island_perimeter(grid) -> int:
-    """This function is aim to determinate the island perimeter"""
-    perim = 0
+    """This function calculate the island perimeter"""
+    perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if (grid[i][j] == 1):
                 if i == 0 or grid[i - 1][j] == 0:
-                    perim += 1
+                    perimeter += 1
                 if i == len(grid) - 1 or grid[i + 1][j] == 0:
-                    perim += 1
+                    perimeter += 1
                 if j == 0 or grid[i][j - 1] == 0:
-                    perim += 1
+                    perimeter += 1
                 if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
-                    perim += 1
-    return perim
+                    perimeter += 1
+    return perimeter
